23a9c402db48db2674825f8526d780043408ac82
Jed Helmers <jedhelmers@gmail.com>
2022-04-03 22:22:23 -0400

    Update AAC2M4H1.v

--- AAC2M4H1/AAC2M4H1.v
+++ AAC2M4H1/AAC2M4H1.v
@@ -3,28 +3,18 @@ module ALU (
   input [31:0] A, B,
   output reg [31:0] Y
 );
-
-  parameter
-    a = 3'b000,
-    add = 3'b001,
-    subtract = 3'b010,
-    aND = 3'b011,
-    oR = 3'b100,
-    incrementA = 3'b101,
-    decrementB  = 3'b110,
-    b = 3'b111;
-
-  always@(A or B or Op_code) begin
+  always@(A or B or Op_code)
+  begin
     case (Op_code)
-      a : Y = A;
-      add : Y = A + B;
-      subtract : Y = A - B;
-      aND : Y = A & B;
-      oR : Y = A | B;
-      incrementA : Y = A + 1;
-      decrementB : Y = B - 1;
-      b : Y = B;
-      default : Y=32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
+      3'b000 : Y = A;
+      3'b001 : Y = A + B;
+      3'b010 : Y = A - B;
+      3'b011 : Y = A & B;
+      3'b100 : Y = A | B;
+      3'b101 : Y = A + 1'b1;
+      3'b110 : Y = B - 1'b1;
+      3'b111 : Y = B;
+      default : Y = {32{1'bz}};
     endcase
   end
 endmodule
